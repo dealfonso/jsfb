1f3691d2c679e47aeebb9c5280113699a294b8bd
Carlos A. <caralla@upv.es>
2024-12-17 16:02:43 +0100

    remove the jsfb and set fb prefix

--- src/js/resizablecolumntable.js
+++ src/js/resizablecolumntable.js
@@ -55,7 +55,7 @@ class ResizableColumnTable {
         }
 
         this.table = table;
-        this.table._jsfbResizableTable = this;
+        this.table._fbResizableTable = this;
         this.headers = headers;
 
         // Create the resizer elements
